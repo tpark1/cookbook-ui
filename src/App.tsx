6c1f179b0bcbb65e4085c20e84802373d3bfa553
Ted Park <tedpark7@gmail.com>
2026-04-20 13:54:32 -0400

    Add supabase client, react router, remove auto generated css

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,14 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import HomePage from "./pages/homePage";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started </h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={async () => {
-            setCount((count) => count + 1);
-            const res = await fetch("http://localhost:3000/");
-            if (res.ok) {
-              const test = await res.json();
-              console.log("test", test);
-            } else {
-              console.log("error while trying to fetch");
-            }
-          }}
-        >
-          Count is {count}
-        </button>
-      </section>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/homePage.tsx
@@ -0,0 +1,11 @@
+const HomePage = () => {
+  return (
+    <div>
+      <section>Filtering section placeholder</section>
+      <section>Active filters section placeholder</section>
+      homepage placeholder
+    </div>
+  );
+};
+
+export default HomePage;
